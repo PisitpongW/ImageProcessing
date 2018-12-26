4962420f1591fc55a7c757502e1b3846fac31471
PisitpongW <38209670+PisitpongW@users.noreply.github.com>
2018-12-26 08:28:37 +0700

    Add files via upload

---
+++ Morphological/dila_eros.py
@@ -0,0 +1,19 @@
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+
+lsy = cv2.imread("D:\Work\Image_Processing\Blog\py_code\lee-sung-kyung.jpg",0)
+kernel = np.ones((4,4),np.uint8)
+
+plt.subplot(1,3,1),plt.imshow(lsy,cmap = 'gray')
+plt.title('original'), plt.xticks([]), plt.yticks([])
+
+dilation = cv2.dilate(lsy,kernel,iterations = 1)
+plt.subplot(1,3,2),plt.imshow(dilation,cmap = 'gray')
+plt.title('dilation'), plt.xticks([]), plt.yticks([])
+
+erosion = cv2.erode(lsy,kernel,iterations = 1)
+plt.subplot(1,3,3),plt.imshow(erosion,cmap = 'gray')
+plt.title('erosion'), plt.xticks([]), plt.yticks([])
+
+plt.show()
---
+++ Morphological/open_clos.py
@@ -0,0 +1,19 @@
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+
+lsy = cv2.imread("D:\Work\Image_Processing\Blog\py_code\lee-sung-kyung.jpg",0)
+kernel = np.ones((4,4),np.uint8)
+
+plt.subplot(1,3,1),plt.imshow(lsy,cmap = 'gray')
+plt.title('original'), plt.xticks([]), plt.yticks([])
+
+opening = cv2.morphologyEx(lsy, cv2.MORPH_OPEN, kernel)
+plt.subplot(1,3,2),plt.imshow(opening,cmap = 'gray')
+plt.title('opening'), plt.xticks([]), plt.yticks([])
+
+closing = cv2.morphologyEx(lsy, cv2.MORPH_CLOSE, kernel)
+plt.subplot(1,3,3),plt.imshow(closing,cmap = 'gray')
+plt.title('closing'), plt.xticks([]), plt.yticks([])
+
+plt.show()
