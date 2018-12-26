d1453f527bbb850db1dd1903fff2d3f7fea0509e
PisitpongW <38209670+PisitpongW@users.noreply.github.com>
2018-12-26 08:39:13 +0700

    Update open_clos.py

--- Morphological/open_clos.py
+++ Morphological/open_clos.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 lsy = cv2.imread("D:\Work\Image_Processing\Blog\py_code\lee-sung-kyung.jpg",0)
-kernel = np.ones((4,4),np.uint8)
+kernel = np.ones((3,3),np.uint8)
 
 plt.subplot(1,3,1),plt.imshow(lsy,cmap = 'gray')
 plt.title('original'), plt.xticks([]), plt.yticks([])
